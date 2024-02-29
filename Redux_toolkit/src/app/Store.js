92dd0a95a0bbf821919481de259ee5bf792b5446
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-02-29 23:42:44 +0530

    Update Store.js

--- Redux_toolkit/src/app/Store.js
+++ Redux_toolkit/src/app/Store.js
@@ -1,5 +1,16 @@
 // firstly configure the store.
-import {configureStore} from '@reduxjs/toolkit' // firslty you have to configure your store.
+import { configureStore } from '@reduxjs/toolkit'; // firslty you have to configure your store.
+import { todoReducer } from '../Features/todolist/todoSlices';
+
 // then create the store and export it.
-export const Store=configureStore();
-// Secondly , you have to create the reducers and in toolkit we say we have to create Slices. which is in the Freatures.
+export const Store = configureStore({
+    // here you have to register all the reducers you have created in key value pairs.
+    // the key represents the name of the state slice that will be managed by this reducer.
+    // the value represents the reducer function itself.
+    reducers: {
+        // the key 'todos' represents the state slice that will be managed by the 'todoReducer' function.
+        todos: todoReducer,
+    },
+});
+
+
