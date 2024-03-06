e43020f80ae5bed6c973700e6f135f6eab30ca77
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-03-07 01:59:12 +0530

    Update AddTodo.jsx

--- Redux_toolkit/src/Component/AddTodo.jsx
+++ Redux_toolkit/src/Component/AddTodo.jsx
@@ -1,28 +1,40 @@
-import React, { useState } from 'react'
-import {useDispatch} from 'react-redux'
-import { addTodo } from '../Features/todolist/todoSlices';
-const  AddTodo = () => {
-  const [inputdata,Setinputdata]=useState("");
-  
-  const dispatch=useDispatch();
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo,updateinput } from "../Features/todolist/todoSlices";
+const AddTodo = () => {
+  const [inputdata, Setinputdata] = useState("");
 
-  const handleSubmit=(e)=>{
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+     dispatch(updateinput(inputdata));
+  },[inputdata])
+  const handleSubmit = (e) => {
     e.preventDefault();
     // now you have to dispatch the data also to the state through the reducers.
     dispatch(addTodo(inputdata));
-    Setinputdata("")
-
-  }
+    Setinputdata("");
+  };
 
   return (
-    <div>
-      <form action="" onSubmit={handleSubmit} className='form_section'>
-        <input type="text" value={inputdata} className='input_field'
-        onChange={(e)=>Setinputdata(e.target.value)} />
-        <button type="submit" className='Submit_btn'>Add</button>
-      </form>
-    </div>
-  )
-}
+    <form onSubmit={handleSubmit} className="space-x-3 mt-12 ">
+      <input
+        type="text"
+        className="bg-gray-800  rounded border border-gray-700
+         focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none
+          text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        placeholder="Enter a Todo..."
+        value={inputdata}
+        onChange={(e) => Setinputdata(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+      >
+        Add Todo
+      </button>
+    </form>
+  );
+};
 
-export default AddTodo
+export default AddTodo;
