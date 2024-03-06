322cb9dd1fdfdb6d3e063687946dd4f3f4af3dfe
pawan kushwaha <107002528+19coderpawan@users.noreply.github.com>
2024-03-07 01:59:31 +0530

    Update Todolist.jsx

--- Redux_toolkit/src/Component/Todolist.jsx
+++ Redux_toolkit/src/Component/Todolist.jsx
@@ -1,29 +1,46 @@
-import React from 'react'
+import React from "react";
 // In this file i am going to list all the todos items and also going to assign the delete button.
 // to list all the item we are going to use the useSelcetor to access the state and then we can map the array that contain the items.
 // Also , i am going to use the dispatch method also beacuse in this file also i have to call the delete action using the reducers.
-import {  useDispatch, useSelector } from 'react-redux'
-import { removeTodo } from '../Features/todolist/todoSlices'
+
+import { useDispatch, useSelector } from "react-redux";
+import { removeTodo ,updateTodo} from "../Features/todolist/todoSlices";
 
 const Todolist = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
 
-  const dispatch=useDispatch();
-  const todosselctor=useSelector((state)=>state.todos)
+  
   return (
     <div>
-      {/* now map the todo list here */}
-      <div>
-      {todosselctor.map((element)=>{
-        <div>
-          <li id={element.id}>
-           {element.text}
-          </li>
-          <button onClick={()=>dispatch(removeTodo(element.id))}>del</button>
-        </div>
-      })}
-      </div>
-    </div>
-  )
-}
+      <div className="text-red">Todos</div>
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+            <div className="flex gap-1">
 
-export default Todolist
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              <i className="fa-solid fa-trash"></i>
+            </button>
+            <button 
+            onClick={()=>dispatch(updateTodo({id:todo.id,text:"pawan"}))}
+            className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md">
+            <i className="fa-regular fa-pen-to-square"></i>
+            </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todolist;
+
